f59651091ff0253f62a99a50674989bcacbd38dd
Jingshuai-cloud <xujingshuai@gmail.com>
2020-09-30 12:40:45 +1000

    Max Area

--- src/main/java/Array/RemoveDuplicates.java
+++ src/main/java/Array/RemoveDuplicates.java
@@ -3,12 +3,14 @@ package Array;
 public class RemoveDuplicates {
     public static void main(String[] args) {
         int[] nums = {1,1,2,3,4,4};
-        System.out.println(removeDuplicates(nums));
+        System.out.println(removeDuplicates2(nums));
 
     }
 
+    //double pointer
     public static int removeDuplicates(int[] nums){
         int j = 0;
+        if(nums.length == 0) return 0;
         for(int i = 1; i < nums.length; i++){
             if(nums[i] != nums[j]){
                 j++;
@@ -16,5 +18,18 @@ public class RemoveDuplicates {
             }
         }
         return j+1 ;
+    }
+
+    // count k
+    public static int removeDuplicates2(int[] nums){
+        int count = 0;
+        int len = nums.length;
+        for(int i = 1; i< len; i++){
+            if(nums[i] == nums [i-1]) count++;
+            else{
+                nums[i-count] = nums[i];
+            }
+        }
+        return len - count;
     }
 }
